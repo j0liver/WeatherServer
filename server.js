595a5ddd74575fe0de6ba7c3bfb9c5ecb986d233
Joe O <j0liverko@gmail.com>
2020-01-13 22:10:17 -0600

    Started server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+// server.use('/api/auth', authRouter);
+// server.use('/api/profile', authenticate, profileRouter);
+// server.use('/api/recipes', recipeRouter);
+// server.use('/api/steps', stepsRouter);
+// server.use('/api/ingridents', ingredientRouter);
+server.get('/', (req, res) => {
+    console.log("its alive")
+    res.status(200).json({api: "is alive"})
+})
+module.exports = server;
